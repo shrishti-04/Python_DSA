91d2b32788575e56cfd852dd0dd906301b998557
Shrishti Tiwari <shrishtisreyansh@gmail.com>
2024-10-27 12:28:23 +0530

    minmaxsum

---
+++ Hackerrank_Ques/ladder.py
@@ -0,0 +1,32 @@
+# Staircase detail
+
+# This is a staircase of size n = 4:
+
+#        #
+#       ##
+#      ###
+#     ####
+# Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+# Write a program that prints a staircase of size n.
+
+# Function Description
+
+# Complete the staircase function in the editor below.
+
+# staircase has the following parameter(s):
+
+# int n: an integer
+# Print
+
+# Print a staircase as described above.
+
+# Input Format
+
+# A single integer, n, denoting the size of the staircase.
+
+n = int(input('Enter your number: '))
+
+for i in range(n):
+    print(' '*(n-i), end='')
+    print('#' * (i+1))
