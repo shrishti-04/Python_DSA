93f7f460f7d8e9c7e2e1dc594ef4baf5de3cc1d6
Shrishti Tiwari <shrishtisreyansh@gmail.com>
2024-12-07 19:45:48 +0530

    selectionSort

---
+++ Sorting/selectionSort.py
@@ -0,0 +1,17 @@
+# Psuedo code
+
+def selectionSort(arr):
+    n = len(arr)
+    for i in range(0, n-1):
+        mini = i
+        for j in range(i, n):
+            if(arr[j] < arr[mini]):
+                mini = j
+        temp = arr[mini]
+        arr[mini] = arr[i]
+        arr[i] = temp
+
+    return arr
+
+result = selectionSort([8, 4, 9, 3, 5, 1])
+print(result)
