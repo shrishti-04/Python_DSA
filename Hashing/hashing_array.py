e33408215fab80ef56e5787960f956aead6e312a
Shrishti Tiwari <shrishtisreyansh@gmail.com>
2024-12-16 13:19:41 +0530

    hashmap

--- Hashing/hashing_array.py
+++ Hashing/hashing_array.py
@@ -1,18 +1,19 @@
 # to find the frequency of the element in array using hashing
 
-n = int(input())
-array = []
-hash_array = [0] * 51
+n = int(input('enter length of arr: '))
+arr = []
+hash_arr = [0]*100
 
 for i in range(n):
-    array.append(int(input()))
-    hash_array[array[i]] += 1
-    
-q = int(input())
+    num = int(input('Enter a number: '))
+    arr.append(num)
+    hash_arr[num] += 1
+
+q = int(input('No. of queries: '))
 
 for j in range(q):
-    query = int(input())
-    count = hash_array[query]
+    element = int(input('Enter no. in query: '))
+    count = hash_arr[element]
     print(count)
     
 # Here the time complexity will be reduced than the brute force solution
--- Hashing/hashmap.py
+++ Hashing/hashmap.py
@@ -1,22 +1,23 @@
 # to find the frequency of the element in array using hashmap.
 
-from collections import defaultdict
-
-n = int(input())
+n = int(input('enter length of arr: '))
 arr = []
-hash_arr = defaultdict(int)
+hash_arr = {}
 
 for i in range(n):
-    num = int(input())
+    num = int(input('Enter a number: '))
     arr.append(num)
-    hash_arr[num] += 1
-    
-q = int(input())
+    if(num in hash_arr):
+        hash_arr[num] += 1
+    else:
+        hash_arr[num] = 1
 
-for i in range(q):
-    query = int(input())
-    count = hash_arr[query]
-    print(count)
+q = int(input('No. of queries: '))
+
+for j in range(q):
+    element = int(input('Enter no. in query: '))
+    count = hash_arr.get(element, 0)
+    print(element, '->', count)
     
 # Here the time complexity will be reduced than the hash array solution
 # that is O(N)
