aec30102bd38be92f617134c5d4a5432f7173b31
Shrishti Tiwari <shrishtisreyansh@gmail.com>
2024-12-26 23:01:02 +0530

    equalNumber

--- Hashing/hashingQues/equalNumber.py
+++ Hashing/hashingQues/equalNumber.py
@@ -1,5 +1,28 @@
 # Check if there are any two Equal numbers in an array at a distance less than or equal to k
 
+# Brute Force
+
+def nearDis(arr, k):
+    n = len(arr)
+    for i in range(n):
+        for j in range(i+1, n):
+            if(arr[i] == arr[j] and j-i <= k):
+                return True
+    return False
+
+def main():
+    arr = [1, 1, 3, 1, 2, 3]
+    k = 2
+
+    if(nearDis(arr, k)):
+        print('Yes')
+    else:
+        print('No')
+
+main()
+
+# Hashing Method
+
 def contains_nearby_duplicate_optimized(nums, k):
     num_indices = {}
 
