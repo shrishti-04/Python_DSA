a1f986a59982af048a55b9c0f04c15babd7c3ea5
Shrishti Tiwari <shrishtisreyansh@gmail.com>
2025-01-18 18:00:37 +0530

    AmazonQuesOA

--- Hashing/hashingQues/AmazonQues/basicOfQues.py
+++ Hashing/hashingQues/AmazonQues/basicOfQues.py
@@ -1,41 +1,41 @@
 # First we will do Basic Question using hashing method
 
-# def maxSubarraySum(b):
-#     n = len(b) - 1
-#     if(n == 0):
-#         return 0
+def maxSubarraySum(b):
+    n = len(b) - 1
+    if(n == 0):
+        return 0
     
-#     P1 = [0] * (n + 1)
+    P1 = [0] * (n + 1)
 
-#     for i in range(1, n+1):
-#         P1[i] = max(P1[i-1] + b[i], b[i], 0)
-#         print(P1[i])
+    for i in range(1, n+1):
+        P1[i] = max(P1[i-1] + b[i], b[i], 0)
+        print(P1[i])
 
-#     maxSum = P1[1]
-#     for i in range(2, n+1):
-#         if(P1[i] > maxSum):
-#             maxSum = P1[i]
+    maxSum = P1[1]
+    for i in range(2, n+1):
+        if(P1[i] > maxSum):
+            maxSum = P1[i]
 
-#     return maxSum
+    return maxSum
 
-# b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# print('Maximum Subarray is:', maxSubarraySum(b))
+b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print('Maximum Subarray is:', maxSubarraySum(b))
 
 # Now we will implement the same using kadane's algorithm
 
-# def maxSubarraySumKadane(b):
-#     n = len(b) - 1
-#     if(n == 0):
-#         return 0
+def maxSubarraySumKadane(b):
+    n = len(b) - 1
+    if(n == 0):
+        return 0
     
-#     T = 0
-#     current = 0
+    T = 0
+    current = 0
 
-#     for i in range(1, n+1):
-#         current = max(current + b[i], b[i], 0)
-#         T = max(T, current)
+    for i in range(1, n+1):
+        current = max(current + b[i], b[i], 0)
+        T = max(T, current)
 
-#     return T
+    return T
 
-# b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# print('Maximum Subarray is:', maxSubarraySumKadane(b))
+b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print('Maximum Subarray is:', maxSubarraySumKadane(b))
